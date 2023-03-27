e8fdb7b00c58cf925913bf8faed1678d41c83c70
Amin Rezaei <AminRezaei0x443@gmail.com>
2023-03-27 21:37:38 +0330

    feature: entailment now captures too much entails

--- autofaq/clean/entailment.py
+++ autofaq/clean/entailment.py
@@ -9,11 +9,20 @@ class EntailmentCleaner(Cleaner):
     def clean(self, df: DataFrame):
         embeddings = torch.load(".cache/embeddings.bin")
         scores_map = {k: torch.dot(v["q"], v["a"]) for k, v in embeddings.items()}
+        self_scores = {k: torch.dot(v["q"], v["q"]) for k, v in embeddings.items()}
+        deviation_map = {k: np.abs(scores_map[k] - self_scores[k]) for k in scores_map}
         ids = list(df["id"])
         scores_map = {k: v for k, v in scores_map.items() if k in ids}
         scores = list(scores_map.values())
-        m = np.mean(scores)
-        var = np.std(scores)
+        m, var = np.mean(scores), np.std(scores)
         print(f"mean: {m}, std: {var}")
-        r = df.apply(lambda x: bool(scores_map[x["id"]] > m), axis=1)
+        deviations = list(deviation_map.values())
+        m_dev, std_dev = np.mean(deviations), np.std(deviations)
+        print(f"dev mean: {m_dev}, dev std: {std_dev}")
+        r = df.apply(
+            lambda x: bool(
+                (scores_map[x["id"]] > m) and (deviation_map[x["id"]] < m_dev)
+            ),
+            axis=1,
+        )
         return r
