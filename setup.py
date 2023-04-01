4b380bbd3dc5b850426bbeb86dda90f3c6579960
Amin Rezaei <AminRezaei0x443@gmail.com>
2023-04-02 03:09:56 +0330

    release: v0.4.1 + use readme contents

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
+
+# Read README contents
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="auto-faq",
-    version="0.4.0",
+    version="0.4.1",
     description="Automatic FAQ Mining Framework",
     license="MIT",
     packages=find_packages(exclude=["test"]),
@@ -17,4 +23,6 @@ setup(
     url="https://github.com/AminRezaei0x443/auto-faq",
     install_requires=[],
     extras_require={},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
