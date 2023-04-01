abfa131c8d3c1699121b1b599c94e2aad7359ba1
Amin Rezaei <AminRezaei0x443@gmail.com>
2023-04-02 02:59:18 +0330

    feature: pypi workflow, release: v0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.4.0",
     description="Automatic FAQ Mining Framework",
     license="MIT",
-    packages=find_packages(),
+    packages=find_packages(exclude=["test"]),
     author="Amin Rezaei",
     author_email="AminRezaei0x443@gmail.com",
     keywords=[],
